9336d6a3b7d16470a1cefe5d29d6bba7d99dffdb
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2021-01-11 14:39:58 +0100

    edit, update, delete

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show, :edit, :update]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
   def index
     @flats = Flat.all
   end
@@ -24,7 +24,13 @@ class FlatsController < ApplicationController
   end
 
   def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
 
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
